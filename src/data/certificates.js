fa4b9bf67b653748c0e5b478719951425abf3c9f
Prajesh Yeotikar <yeotikarprajesh@gmail.com>
2023-05-21 08:43:08 +0530

    Add files via upload

---
+++ src/data/certificates.js
@@ -0,0 +1,323 @@
+
+const data = [
+  
+  {
+    id: '1',
+    title: 'Google Cybersecurity Professional ',
+    description:'',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/new/Google%20Cybersecurity.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     ' I completed the Google Cybersecurity Professional Certificate on Coursera. I learned about cybersecurity threats, vulnerabilities, and mitigation techniques. I also gained hands-on experience with industry standard tools like Python, Linux, and Security Information and Event Management (SIEM) programs.'
+    ],
+  },
+  {
+    id: '2',
+    title: 'Certified in Cybersecurity (1M Self-Paced) by ISC2',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/new/CC%20ISC2.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     'I completed the Certified in Cybersecurity (CySA+) course from ISC2. I learned about the fundamentals of cybersecurity, including threat analysis, risk assessment, and incident response. I also gained hands-on experience with industry standard tools and techniques.'
+    ],
+  },
+
+  {
+    id: '3',
+    title: 'Getting Started with Vulnerability Analysis and Management',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/getting%20started%20with%20vuln%20analysis.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+  
+  {
+    id: '4',
+    title: 'In the Trenches: Security Operations Center',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/In-the-trenches.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+  
+  {
+    id: '5',
+    title: ' Ethical Hacking/Penetration Testing & Bug Bounty Hunting',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/Ethical%20Hacking%2C%20PT%20rohit%20gautam.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+    {
+    id: '6',
+    title: ' Ethical Hacking/Penetration Testing & Bug Bounty Hunting v2',
+    description:
+      '',
+    image: {
+      // src: 'https://raw.githubusercontent.com/prajeshy/certs/main/v2-1.png',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/ehpt-v2.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+  
+  {
+    id: '7',
+    title: 'Google : Crash Course on Python',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/python-crash-course.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+    
+  
+  {
+    id: '8',
+    title: 'Ethical Hacking and Cybersecurity',
+    description:
+      '',
+    image: {
+      // src: 'https://res.cloudinary.com/dx6tl6aa2/image/upload/v1628348072/portfolio/projects/react-marketplace/apiko-marketplace-lg_xppe9n.jpg',
+      src: 'https://ollialic.sirv.com/Images/Portfolio/EHCS.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apiko-marketplace-app-snaichuk.herokuapp.com/',
+      repo: 'https://github.com/vsnaichuk/apiko-marketplace-app-snaichuk',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '9',
+    title: 'Linux for Network Engineers Practical Linux with GSN3',
+    description:
+     '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/linux-for-network-eng.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://yourplaces-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/MERN-places',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '10',
+    title: 'Complete CCNP ENCOR (350-401) Master Class',
+    description:
+     '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/CCNP.jpg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/online-shop-app',
+      repo: 'https://github.com/vsnaichuk/RN-shop',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '11',
+    title: 'Usable Security',
+    description:
+      '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/usable-security.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/rn-places',
+      repo: 'https://github.com/vsnaichuk/RN-places',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '12',
+    title: 'Wireshark for Basic Network Security Analysis',
+    description:
+      '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/new/wireshark.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/rn-places',
+      repo: 'https://github.com/vsnaichuk/RN-places',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '13',
+    title: 'Database Foundation by Oracle Academy',
+    description:
+      '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/new/course_certificate-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/rn-places',
+      repo: 'https://github.com/vsnaichuk/RN-places',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '11',
+    title: 'Blockchain Specialization ',
+    description:
+      '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/new/blockchain-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://expo.dev/@v_snaichuk/rn-places',
+      repo: 'https://github.com/vsnaichuk/RN-places',
+    },
+    technologies: [
+     
+    ],
+  },
+  {
+    id: '12',
+    title: 'Object Orineted Programming in Java',
+    description:
+      '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/Coursera%20W23HYFYD5TCW-2-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+      
+    ],
+  },
+  {
+    id: '13',
+    title: 'Java Programming : Solving Problem using Software',
+    description:
+    '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/probmlem%20solving%20java-1.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+    ],
+  },
+  {
+    id: '14',
+    title: 'C++ : From Beginner to Expert',
+    description:
+   '',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/cpp.jpeg',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://shopping-cart-sv.netlify.app/',
+      repo: 'https://github.com/vsnaichuk/React-ShoppingCart',
+    },
+    technologies: [
+    
+    ],
+  },
+];
+ export default data;
---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: '1',
+    title: 'Azure Sentinel RDP Defense',
+    description:
+      'Geolocation data extraction from Windows Event Viewer using PowerShell and visualization of attacks in Azure Sentinel.',
+    image: {
+      src: 'https://ollialic.sirv.com/Images/Portfolio/Screenshot%202023-05-20%20at%208.30.54%20PM.png',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://apps.apple.com/us/app/syntho/id1615419392',
+    },
+    technologies: [
+      'Powershell scripting',
+      'azure',
+      'Log Analytics Workspace',
+      'Azure Sentinel',
+      'API',
+      'Geolocation Extraction Tools',
+      
+    ],
+  },
+  
+];
+
+export default data;
