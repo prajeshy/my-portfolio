c97bd3c22e527879022bff93054a9d94f878d369
Prajesh Yeotikar <yeotikarprajesh@gmail.com>
2023-06-14 23:09:42 +0530

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -22,6 +22,24 @@ const data = [
       
     ],
   },
+   {
+    id: '2',
+    title: 'Dorker - A Google Dorking tool',
+    description:
+      'Unleash the power of advanced dorking techniques. Discover leaked credentials, exploit GitHub and Google dorks, uncover SQL errors, and more. Empower your information gathering and cybersecurity efforts with our intuitive platform. Explore the hidden depths of the digital realm and stay ahead of potential vulnerabilities.',
+    image: {
+      src: 'https://upcdn.io/kW15bRZ/raw/ezgif-2-4e50b088ea.gif',
+      placeholderSrc:
+        'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAzIDIiPjwvc3ZnPg==',
+    },
+    links: {
+      site: 'https://github.com/prajeshy/dorker',
+    },
+    technologies: [
+      'Python',
+      'Google Dorking',  
+    ],
+  },
   
 ];
 
